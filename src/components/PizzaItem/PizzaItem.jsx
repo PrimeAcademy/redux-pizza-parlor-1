0607590deb700285161ef166d259536347f53c63
Eric Meinzer <emeinzer7@gmail.com>
2021-10-28 14:28:36 -0500

    PizzaMenu css and info added to component

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -4,9 +4,11 @@ import './PizzaItem.css'
 function PizzaItem({pizzaItem}) {
     console.log('this is the pizzaItem', pizzaItem.image_path);
     return(
-        <div className="pizz-card">
+        <div className="pizza-card">
             <img src={pizzaItem.image_path} />
-            <p>pizza info goes here!</p>
+            <h4>{pizzaItem.name}</h4>
+            <p>{pizzaItem.description}</p>
+            <h5>${pizzaItem.price}</h5>
             <button>ADD/REMOVE</button>
         </div>
     )
