20be72c7402dbc44825c110f9ce38db68d315bfc
Eric Meinzer <emeinzer7@gmail.com>
2021-10-28 13:04:11 -0500

    GET route set; PizzaMenue and PizzaList;

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+// import { useDispatch } from 'react-redux';
+import PizzaMenu from '../PizzaMenu/PizzaMenu.jsx'
+
 
 function App() {
+
+  const [pizzaList, getPizzaList] = useState([]);
+
+  function getPizzas() {
+    axios({
+      method: `GET`,
+      url: `/api/pizza`
+    })
+      .then(response => {
+        console.log(`GET /api/pizza response`, response.data);
+        getPizzaList(response.data)
+      })
+      .catch(error => {
+        console.log(`GET /api/pizza ERROR`, error);
+      })
+  };
+
+
+
+  useEffect(() => {
+    getPizzas();
+  }, [])
+
+
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
+
+      <PizzaMenu pizzaList={pizzaList} />
+
+      {/* <img src='images/pizza_photo.png' /> */}
+      
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+// import {Provider} from 'react-redux'
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// ReactDOM.render(
+//     <Provider store="storeInstance">
+//         <App />
+//     </Provider>, 
+// document.getElementById('root'));
